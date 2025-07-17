e98e09c6f883cd0587443aa52757662435675b60
SabirGuliyev <sabir.guliyev@code.edu.az>
2025-07-17 23:09:14 +0400

    Condition added

---
+++ OperatorsExample/OperatorsExample/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int age = 15;
+string gender = "female";
+
+if (age >= 17)
+{
+    if (gender == "female")
+    {
+        Console.WriteLine("Ishe qebul olduz");
+    }
+    else
+    {
+        Console.WriteLine("Xanim ishcisi axtaririq");
+    }
+}
+else
+{
+    Console.WriteLine("Yashiniz uygun deyil");
+}
+
