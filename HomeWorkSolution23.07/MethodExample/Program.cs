cc29b89e199efb5219228c92e63748e5c4f3b856
SabirGuliyev <sabir.guliyev@code.edu.az>
2025-07-24 22:44:11 +0400

    Method added

---
+++ HomeWorkSolution23.07/MethodExample/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System;
+
+//int[] nums = { 0,11, 13, 20, 55, 127,27 };
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] < 2)
+//    {
+//        continue;
+//    }
+//    for (int j = 2; j * j <= nums[i]; j++)
+//    {
+//        if (nums[i] % j == 0)
+//        {
+//            Console.WriteLine(nums[i] + " Murekkebdir");
+//            break;
+//        }
+//    }
+//}
+
+
+
+int[] numbers = { 288, 34, 1, -5, 9, 7 ,-5,34};
+
+int step = 0;
+for (int i = 0; i < numbers.Length-1; i++)
+{
+    step++;
+    if (numbers[i] > numbers[i + 1])
+    {
+        //a=5  b=10   a+=b  a=15  b=5   a=10
+        numbers[i] += numbers[i + 1];
+        numbers[i+1]= numbers[i]-numbers[i+1];
+        numbers[i] = numbers[i]-numbers[i+1];
+        i = -1;
+    }
+}
+
+
+for (int i = 0;i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+Console.WriteLine("Addimlar: "+step);
---
+++ MethodExampe2/MethodExampe2/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace MethodExampe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int product = 90;
+            //int discount = 10;
+            //Console.WriteLine(product*discount/100);
+
+            //int product2 = 120;
+            //int discount2 = 20;
+            //Console.WriteLine(product2*discount2/100);
+            //int product = 150;
+
+
+            //int product = 150;
+
+            //int discountedValue = GetDiscount(product, 20);
+
+            //product-=discountedValue;
+            //Console.WriteLine(product);
+
+
+
+
+
+
+
+            //int num = 24;
+
+            //    if (IsEven(num))
+            //    {
+            //        Console.WriteLine("Cutdur");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Tekdir");
+            //    }
+
+
+
+            //int[] nums = { 12, 15, 18 };
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (IsEven(nums[i]))
+            //    {
+            //        Console.WriteLine("Cutdur");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Tekdir");
+            //    }
+            //}
+
+
+            //GetFullname("Test");
+
+
+            //string[] text = { "Salam", "Necesiz", "Gencler" };
+            //CreateSentence(text);
+
+            CreateSentence("Salam", "Gencler", "Necesiz");
+            CreateSentence();
+        }
+
+        public static void CreateSentence(params string[] words)
+        {
+            string sentence = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                sentence+= words[i];
+                sentence+= " ";
+            }
+
+            Console.WriteLine(sentence);
+        }
+
+
+
+
+
+        public static void GetFullname(string name,string surname="xxx")
+        {
+            Console.WriteLine(name+" "+surname);
+        }
+
+
+        public static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+               
+            }
+            return false;
+        }
+
+
+
+        public static int GetDiscount(int price, int percent)
+        {
+            int result = price * percent / 100;
+            return result;
+
+        }
+    }
+}
