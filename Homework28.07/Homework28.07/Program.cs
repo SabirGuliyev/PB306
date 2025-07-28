ee1bf3620fc568c5aafae985857f9ebd63615316
SabirGuliyev <sabir.guliyev@code.edu.az>
2025-07-28 22:56:42 +0400

    Method tasks added

---
+++ Homework28.07/Homework28.07/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Homework28._07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetPower(5,1);
+
+        }
+        public static int GetMin(int[] nums)
+        {
+            int min = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (min > nums[i])
+                {
+                    min= nums[i];
+                }
+            }
+            return min;
+        }
+
+        public static int[] SortArray(int[] nums)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for(int j = 0; j < nums.Length - 1-i; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        int temp= nums[j];
+                        nums[j]= nums[j + 1];
+                        nums[j+1]= temp;
+                    }
+                }
+            }
+            return nums;
+        }
+
+        public static void GetPower(int num,int pow=2)
+        {
+            int result = 1;
+            for (int i = 0; i < pow; i++)
+            {
+               result *= num;
+            }
+            Console.WriteLine(result);
+        }
+    }
+
+}
