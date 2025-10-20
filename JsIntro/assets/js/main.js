5d03f5343b6d0118b445ead12bf92415946adeac
SabirGuliyev <sabir.guliyev@code.edu.az>
2025-10-20 22:29:14 +0400

    js intro and scss added

---
+++ JsIntro/assets/js/main.js
@@ -0,0 +1,127 @@
+// var age;
+
+
+// let age=18;
+// let age=20;
+
+// let age=18;
+
+// age=19;
+
+// console.log(age)
+
+
+
+
+// const fincode="aaaaaaa";
+
+// fincode="bbbbbbb"
+
+// console.log(fincode)
+
+// let const block scope keyword
+//var function scope keyword
+
+
+// if(true){
+//     var name="test"
+// }
+
+// function SayHello(){
+//     var test="test";
+// }
+
+// SayHello();
+
+// console.log(test);
+
+
+
+
+// if(age>18){
+//   var  age=30;
+// }
+
+
+// console.log(age)
+// let age=30;
+
+
+
+
+// console.log(age) //hoisting
+// age=20;
+
+// let name="Test"
+// let age=18;
+
+// console.log(`Salam ${name} age:${age}`)
+
+
+// let num=4n;
+
+// console.log(Number.MAX_VALUE)
+// console.log(typeof(num))
+
+// const user={
+//     userName:"Killer71",
+//     Name:"Test",
+//     Age:18
+// }
+
+
+// user.Name="Test2"
+// age=18;
+
+
+// console.log(age)
+// let user=null;
+
+// console.log(typeof(user))
+// ///   00x0011 -> 00x0012
+
+// //{
+// //    userName:"Killer",
+// //     Name:"Test4",
+// //     Age:19
+
+// // }
+
+
+// console.log(user)
+
+
+
+
+
+// const user2=new Object.create({
+//     userName:"Killer71",
+//     Name:"Test",
+//     Age:18
+// })
+
+
+// function Person(name,surname,age){
+
+//     this.name=name
+//     this.surname=surname
+
+//     this.age=age
+// }
+
+
+// const user3=new Person("Test","Testov",18)
+// console.log(user)
+
+
+
+
+
+function Sum(num){
+
+    num()
+
+}
+
+
+Sum(()=>console.log("salam"))
